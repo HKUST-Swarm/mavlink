7bf31d81ded7ca2daa3385ec0a2734d42a544825
XU Hao <xuhao3e8@gmail.com>
2020-02-26 20:45:00 +0800

    Modified for 2d detection with inv depth

--- gen.sh
+++ gen.sh
@@ -2,7 +2,7 @@
 PROTOCOL_VER=1.0
 mavgen.py  --lang=C --wire-protocol=$PROTOCOL_VER --output=/home/xuhao/swarm_ws/src/swarm_pilot/include/mavlink message_definitions/v1.0/swarm.xml --no-validate
 
-mavgen.py  --lang=C --wire-protocol=$PROTOCOL_VER --output=/home/xuhao/swarm_ws/src/swarm_pkgs/localization_proxy/include/mavlink message_definitions/v1.0/swarm.xml --no-validate
+mavgen.py  --lang=C --wire-protocol=$PROTOCOL_VER --output=/home/xuhao/swarm_ws/src/swarm_localization/localization_proxy/include/mavlink message_definitions/v1.0/swarm.xml --no-validate
 
 mavgen.py --lang=Python --wire-protocol=$PROTOCOL_VER --output=/home/xuhao/swarm_ws/src/swarm_station/scripts/pymavlink4swarm message_definitions/v1.0/swarm.xml --no-validate
 
