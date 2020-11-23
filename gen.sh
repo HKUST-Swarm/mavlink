c507934a3b592fa096e492e055a33a285e510693
XU Hao <xuhao3e8@gmail.com>
2020-11-23 14:39:47 +0800

    Update

--- gen.sh
+++ gen.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 PROTOCOL_VER=1.0
-mavgen.py  --lang=C --wire-protocol=$PROTOCOL_VER --output=/home/xuhao/swarm_ws/src/swarm_pilot/include/mavlink message_definitions/v1.0/swarm.xml --no-validate
-
-mavgen.py  --lang=C --wire-protocol=$PROTOCOL_VER --output=/home/xuhao/swarm_ws/src/swarm_localization/localization_proxy/include/mavlink message_definitions/v1.0/swarm.xml --no-validate
-
-mavgen.py --lang=Python --wire-protocol=$PROTOCOL_VER --output=/home/xuhao/swarm_ws/src/swarm_station/scripts/pymavlink4swarm message_definitions/v1.0/swarm.xml --no-validate
-
+MAV_DEF=message_definitions/v1.0/swarm.xml
+SWARM_WS=/home/xuhao/swarm_ws/
+mavgen.py  --lang=C --wire-protocol=$PROTOCOL_VER --output=$SWARM_WS/src/swarm_pilot/include/mavlink $MAV_DEF --no-validate
+mavgen.py  --lang=C --wire-protocol=$PROTOCOL_VER --output=$SWARM_WS/src/swarm_localization/localization_proxy/include/mavlink $MAV_DEF--no-validate
+mavgen.py --lang=Python --wire-protocol=$PROTOCOL_VER --output=$SWARM_WS/src/swarm_station/scripts/pymavlink4swarm $MAV_DEF --no-validate
+mavgen.py --lang=Python --wire-protocol=$PROTOCOL_VER --output=$SWARM_WS/src/Fast-Swarm-Planner/fast_planner/plan_manage/scripts/pymavlink4swarm $MAV_DEF --no-validate
 #mavgen.py --lang=Python --wire-protocol=$PROTOCOL_VER --output=/home/xuhao/swarm_ws/src/swarm_pilot/scripts/pymavlink4swarm message_definitions/v1.0/swarm.xml --no-validate
-
-mavgen.py --lang=Python --wire-protocol=$PROTOCOL_VER --output=/home/xuhao/swarm_ws/src/uwb_mocap_broadcast/mocap_optitrack/scripts/pymavlink4swarm message_definitions/v1.0/swarm.xml --no-validate
-
-mavgen.py --lang=JavaScript --wire-protocol=$PROTOCOL_VER --output=/home/xuhao/swarm_ws/swarm_gcs/libs/mavlink message_definitions/v1.0/swarm.xml --no-validate
+mavgen.py --lang=Python --wire-protocol=$PROTOCOL_VER --output=$SWARM_WS/src/uwb_mocap_broadcast/mocap_optitrack/scripts/pymavlink4swarm $MAV_DEF--no-validate
+mavgen.py --lang=JavaScript --wire-protocol=$PROTOCOL_VER --output=$SWARM_WS/swarm_gcs/libs/mavlink $MAV_DEF --no-validate
