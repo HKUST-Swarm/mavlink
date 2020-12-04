11dc422edb69e5f6e63015cf0fc814985e51433f
XU Hao <xuhao3e8@gmail.com>
2020-12-04 16:51:58 +0800

    Update NODE_DETECTED

--- gen.sh
+++ gen.sh
@@ -5,7 +5,7 @@ SWARM_WS=/home/xuhao/swarm_ws/
 mavgen.py  --lang=C --wire-protocol=$PROTOCOL_VER --output=$SWARM_WS/src/swarm_pilot/include/mavlink $MAV_DEF --no-validate
 mavgen.py  --lang=C --wire-protocol=$PROTOCOL_VER --output=$SWARM_WS/src/swarm_localization/localization_proxy/include/mavlink $MAV_DEF --no-validate
 mavgen.py --lang=Python --wire-protocol=$PROTOCOL_VER --output=$SWARM_WS/src/swarm_station/scripts/pymavlink4swarm $MAV_DEF --no-validate
-mavgen.py --lang=Python --wire-protocol=$PROTOCOL_VER --output=$SWARM_WS/src/Fast-Swarm-Planner/fast_planner/plan_manage/scripts/pymavlink4swarm $MAV_DEF --no-validate
+#mavgen.py --lang=Python --wire-protocol=$PROTOCOL_VER --output=$SWARM_WS/src/Fast-Swarm-Planner/fast_planner/plan_manage/scripts/pymavlink4swarm $MAV_DEF --no-validate
 #mavgen.py --lang=Python --wire-protocol=$PROTOCOL_VER --output=/home/xuhao/swarm_ws/src/swarm_pilot/scripts/pymavlink4swarm message_definitions/v1.0/swarm.xml --no-validate
-mavgen.py --lang=Python --wire-protocol=$PROTOCOL_VER --output=$SWARM_WS/src/uwb_mocap_broadcast/mocap_optitrack/scripts/pymavlink4swarm $MAV_DEF--no-validate
+#mavgen.py --lang=Python --wire-protocol=$PROTOCOL_VER --output=$SWARM_WS/src/uwb_mocap_broadcast/mocap_optitrack/scripts/pymavlink4swarm $MAV_DEF --no-validate
 mavgen.py --lang=JavaScript --wire-protocol=$PROTOCOL_VER --output=$SWARM_WS/swarm_gcs/libs/mavlink $MAV_DEF --no-validate
